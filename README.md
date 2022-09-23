8275732874f36c08e8306139f6f1375b66cb8a5a
Carson <jackparadise520a@gmail.com>
2022-09-23 17:38:16 +0800

    Add the Usage (#1)

--- README.md
+++ README.md
@@ -3,3 +3,32 @@
 這是一個[goldmark](https://github.com/yuin/goldmark)的擴充功能
 
 解析的對象為 `:joy:` 等類似範本
+
+## USAGE
+
+```go
+package main
+
+import (
+  "bytes"
+  "fmt"
+  emoji "github.com/CarsonSlovoka/goldmark-emoji/v2"
+  "github.com/yuin/goldmark"
+  "os"
+)
+
+func Example() {
+  markdown := goldmark.New(
+    goldmark.WithExtensions(
+      emoji.NewEmojiExtender(),
+    ),
+  )
+
+  markdown.Convert([]byte(":smiling_face_with_smiling_eyes:"), os.Stdout)
+
+  // Output:
+  // <p>😊</p>
+}
+```
+
+更多範例請參考[ExampleNewEmojiExtender](./v2/main_test.go)的代碼
--- v2/main_test.go
+++ v2/main_test.go
@@ -1,9 +1,12 @@
 package emoji_test
 
 import (
+	"bytes"
+	"fmt"
 	emoji "github.com/CarsonSlovoka/goldmark-emoji/v2"
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/testutil"
+	"os"
 	"testing"
 )
 
@@ -14,4 +17,26 @@ func TestNewEmojiExtender(t *testing.T) {
 		),
 	)
 	testutil.DoTestCaseFile(markdown, "test/emoji.txt", t)
+}
+
+func ExampleNewEmojiExtender() {
+	markdown := goldmark.New(
+		goldmark.WithExtensions(
+			emoji.NewEmojiExtender(),
+		),
+	)
+
+	if err := markdown.Convert([]byte(":smiling_face_with_smiling_eyes:"), os.Stdout); err != nil {
+		panic(err)
+	}
+
+	out := bytes.NewBuffer(make([]byte, 0))
+	content := []byte(`Hello World. :smiling_face_with_smiling_eyes: :微笑:`)
+	if err := markdown.Convert(content, out); err != nil {
+		panic(err)
+	}
+	fmt.Println(out)
+	// Output:
+	// <p>😊</p>
+	// <p>Hello World. 😊 😊</p>
 }
