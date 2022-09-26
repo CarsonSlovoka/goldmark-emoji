183c7424df3efc0dcaf334a21044dfb452598061
Carson <jackparadise520a@gmail.com>
2022-09-26 11:07:16 +0800

    創建gen-github-emoji.go來產生GitHub的emoji清單 (#4)

--- v2/def/github.go
+++
@@ -1,9 +0,0 @@
-package def
-
-// Github https://emojipedia.org/man-red-hair/
-func Github() Emojis {
-	return NewEmojis(
-		NewEmoji("man: red hair", []rune{0x1F468, 0x200D, 0x1F9B0}, "red_haired_man", "man_red_hair"),
-		NewEmoji("😊", []rune{0x1F60A}, "blush", "smiling_face_with_smiling_eyes", "微笑"),
-	)
-}
