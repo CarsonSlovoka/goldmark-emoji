8b71222ab59fe02cab301a325d4d79d7f186e45e
Carson <jackparadise520a@gmail.com>
2022-09-26 12:11:34 +0800

    允許加入多個自定義的表情符號組 (#2)

--- v2/main.go
+++ v2/main.go
@@ -8,10 +8,25 @@ import (
 	"github.com/yuin/goldmark/util"
 )
 
-type emojiExtender struct{}
+type emojiExtender struct {
+	emojis def.Emojis
+}
 
-func NewEmojiExtender() goldmark.Extender {
-	return &emojiExtender{}
+var DefaultEmojis def.Emojis
+
+func init() {
+	DefaultEmojis = def.Github()
+}
+
+func NewEmojiExtender(elems ...def.Emojis) goldmark.Extender {
+	if len(elems) == 0 {
+		elems = []def.Emojis{DefaultEmojis}
+	}
+	emojis := elems[0]
+	for _, es := range elems[1:] {
+		emojis.Add(&es)
+	}
+	return &emojiExtender{emojis}
 }
 
 // Extend adds a hashtag parser to a Goldmark parser
@@ -23,7 +38,7 @@ func (e *emojiExtender) Extend(m goldmark.Markdown) {
 		// https://github.com/yuin/goldmark/blob/20df1691ad91648cd872e6472301d7f6d325b448/parser/parser.go#L594-L610
 		parser.WithInlineParsers(
 			util.Prioritized( // {any, priority} 它的any指的是一個對象，一個當被Parse成功之後，可能會返回的產物(包含在ast.Node之中)
-				NewEmojiParser(def.Github()), 1000,
+				NewEmojiParser(e.emojis), 1000,
 			),
 		),
 	)
